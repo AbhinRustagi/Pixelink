0d809f1dbb221b4c0fe52c3db28b61e7ac042e3f
AbhinRustagi <rustagi.abhin@gmail.com>
2024-07-21 15:45:28 +1000

    fix: next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,10 @@ const nextConfig = {
   webpack: (config, { isServer }) => {
     if (!isServer) {
       // Ensure that all imports of 'yjs' resolve to the same instance
-      config.resolve.alias["yjs"] = path.resolve(__dirname, "node_modules/yjs");
+      config.resolve.alias["yjs"] = path.resolve(
+        process.cwd(),
+        "node_modules/yjs"
+      );
     }
     return config;
   },
